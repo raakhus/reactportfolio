304ae881f2faa470f88e3a39047db9e76ba4ec4b
randy <randyaakhus@gmail.com>
2018-11-16 18:07:23 -0800

    working db

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Col,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+
+} from 'reactstrap';
+
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+<Col className="d-flex justify-content-center text-align-center">
+        <Navbar  light expand="md">
+          <NavbarToggler onClick={this.toggle} />
+          
+          <Collapse isOpen={this.state.isOpen} navbar >
+            <Nav className="mrx-auto" navbar pullright>
+              <NavItem className="d-flex align-items-center">
+                <NavLink className="font-weight-bold" href="/">Home</NavLink>
+              </NavItem>
+              <NavItem className="d-flex align-items-center">
+                <NavLink className="font-weight-bold" href="/contact">Contact</NavLink>
+              </NavItem>
+              <NavItem className="d-flex align-items-center">
+                <NavLink className="font-weight-bold" href="/portfolio">Portfolio</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+          
+        </Navbar>
+        </Col>
+      </div>
+    );
+  }
+}
