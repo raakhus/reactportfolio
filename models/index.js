ccafbb33301206676b0d21d839e67925e538d58f
randy <randyaakhus@gmail.com>
2018-11-16 17:32:21 -0800

    asd

--- models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  Contact: require("./contact")
-};
--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const contactRoutes = require("./contact");
-
-
-router.use("/contacts1", contactRoutes);
-
-module.exports = router;
